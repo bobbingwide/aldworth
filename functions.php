8d9e48454665f084324da641fedf263af10511a0
Herb Miller <herb@bobbingwide.com>
2018-04-25 09:33:16 +0100

    Add custom.css - may not be needed and test logic for image quality figure

--- functions.php
+++ functions.php
@@ -10,3 +10,22 @@ function avada_lang_setup() {
 	load_child_theme_textdomain( 'aldworth', $lang );
 }
 add_action( 'after_setup_theme', 'avada_lang_setup' );
+
+
+
+
+/**
+ * Traces the quality value used for images
+ * 
+ * @param integer $quality expected to be between 1 and 100
+ * @param string $mime_type image type
+ * @return integer the image quality
+ */
+function aldworth_editor_set_quality( $quality, $mime_type ) {
+	//bw_trace2();
+	return $quality;
+}
+add_filter( "wp_editor_set_quality", "aldworth_editor_set_quality", 9999, 2 );
+
+
+
